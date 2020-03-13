48290cb55e50b85e8c116fc19e46d9b67acec88a
Alexander Loktionov <a.loktionov@omprussia.ru>
2020-03-13 12:38:39 +0300

    [tpl] Update test monolingual qt template

---
+++ templates/qt-monolingual.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="first-entry">
+        <location filename="first.qml" line="6"/>
+        <source>First</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="second-entry">
+        <location filename="second.qml" line="27"/>
+        <source>Second</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
