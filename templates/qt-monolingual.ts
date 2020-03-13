911b41d2bc78b51fb22a832abe01a6c68d29e049
Alexander Loktionov <a.loktionov@omprussia.ru>
2020-03-13 14:04:49 +0300

    [tpl] Remove all except "good" message from template

--- templates/qt-monolingual.ts
+++ templates/qt-monolingual.ts
@@ -3,22 +3,6 @@
 <TS version="2.1">
 <context>
     <name></name>
-    <message id="some-qt-monoligual">
-        <location filename="QTTranslations.qml" line="6"/>
-        <source>Mist</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="another-qt-monoligual">
-        <location filename="QTTranslationsUpdate.qml" line="27"/>
-        <source>Template update test</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message id="id-back-not-front">
-        <source>Back</source>
-        <extracomment>The back of the object, not the front</extracomment>
-        <translation type="unfinished"></translation>
-        <extra-Context>Not related to back-stepping</extra-Context>
-    </message>
     <message id="second-entry">
         <location filename="second.qml" line="27"/>
         <source>Second</source>
