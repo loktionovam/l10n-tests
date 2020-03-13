88dc632a96bb3c98c3e79900fbb5b96b87f905cc
Alexander Loktionov <a.loktionov@omprussia.ru>
2020-03-13 15:47:37 +0300

    [tpl] Qtlinguist example template
    Text ID Based translation from
        https://doc.qt.io/qt-5/linguist-id-based-i18n.html

--- templates/qt-monolingual.ts
+++ templates/qt-monolingual.ts
@@ -3,10 +3,11 @@
 <TS version="2.1">
 <context>
     <name></name>
-    <message id="second-entry">
-        <location filename="second.qml" line="27"/>
-        <source>Second</source>
+    <message id="id-back-not-front">
+        <source>Back</source>
+        <extracomment>The back of the object, not the front</extracomment>
         <translation type="unfinished"></translation>
+        <extra-Context>Not related to back-stepping</extra-Context>
     </message>
 </context>
 </TS>
