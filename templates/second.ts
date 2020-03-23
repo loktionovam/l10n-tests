d5a77c5ea9c5d97a6c82783b6ac179a8faf7dc40
Aleksandr Loktionov <loktionovam@gmail.com>
2020-03-23 19:07:00 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Update both templates""""""""
    This reverts commit 171f872a8bce2473104adde61e8d6dbd70e67462.

--- templates/second.ts
+++ templates/second.ts
@@ -10,7 +10,7 @@
     </message>
     <message id="second-component-2">
         <location filename="SecondComponent.qml" line="67"/>
-        <source>Third</source>
+        <source>Third from second</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
