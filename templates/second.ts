cc52b2a30c408537b8309c2914721fbc7017f274
Aleksandr Loktionov <loktionovam@gmail.com>
2020-03-23 18:02:25 +0300

    Revert "Add entry to second component"
    This reverts commit f93bb596360ecd3368366fed9a415c4bef2258d7.

--- templates/second.ts
+++ templates/second.ts
@@ -8,10 +8,5 @@
         <source>Second</source>
         <translation type="unfinished"></translation>
     </message>
-    <message id="second-component-3">
-        <location filename="SecondComponent2.qml" line="6"/>
-        <source>Second component 3</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 </TS>
