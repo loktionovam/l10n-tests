e37269239bc969df436eb8f5b592749cb970af91
Aleksandr Loktionov <loktionovam@gmail.com>
2020-03-23 17:40:37 +0300

    Add second translation component

---
+++ templates/second.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="second-component-1">
+        <location filename="SecondComponent.qml" line="6"/>
+        <source>Second</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
